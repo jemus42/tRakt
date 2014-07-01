7a1c7bd406ea55fb8cd430a4b67a1be84820f97c
jemus42 <jemus42@quantenbrot.de>
2014-07-01 14:09:40 +0200

    Initial commit

---
+++ R/trakt.show.summary.R
@@ -0,0 +1,30 @@
+#' Get a show's summary info
+#'
+#' \code{trakt.show.summary} pulls show summary data and returns it
+#' 
+
+#' @param target The \code{slug} or \code{tvdbid} of the show requested
+#' @param apikey API-key used for the call. Defaults to \code{getOption("trakt.apikey")}
+#' @param extended Whether extended info should be provided. Defaults to \code{NULL}, can be 
+#' \code{"normal"} or \code{"full"}
+#' @return A \code{data.frame} containing summary info
+#' @export
+#' @import httr
+#' @note See \href{http://trakt.tv/api-docs/show-summaries}{the trakt API docs for further info}
+#' @examples
+#' \dontrun{
+#' options(trakt.apikey = jsonlite::fromJSON("key.json")$apikey)
+#' breakingbad.summary <- trakt.show.summary("breaking-bad")
+#' }
+trakt.show.summary <- function(target, apikey = getOption("trakt.apikey"), extended = NULL){
+  if (is.null(apikey)){
+    stop("No API key set")
+  }
+  baseURL <- "http://api.trakt.tv/show/summaries.json/"
+  url     <- paste0(baseURL, apikey, "/", target)
+  if (!is.null(extended)){
+    url   <- paste0(url, "/", extended)
+  }
+  response <- jsonlite::fromJSON(url)
+  return(response)
+}
