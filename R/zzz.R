3cb4e98608aec1e76743bb8a70ed064e87cc622c
jemus42 <jemus42@quantenbrot.de>
2015-02-10 13:51:06 +0100

    Remove stored credentials on package detach

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,11 @@
-.onAttach <- function(...) {
+.onAttach <- function(...){
   packageStartupMessage("tRakt is currently under heavy development, so... be careful.")
   tRakt::get_trakt_credentials()
+}
+
+.onDetach <- function(...){
+  options(trakt.client.secret = NULL)
+  options(trakt.client.id     = NULL)
+  options(trakt.apikey        = NULL)
+  options(trakt.username      = NULL)
 }
