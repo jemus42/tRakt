227b2fbccec550896ce09681835f6b1071d0bd3d
jemus42 <jemus42@quantenbrot.de>
2015-02-13 02:45:59 +0100

    Update trakt.show.stats for APIv2 even though NYI

--- R/trakt.show.stats.R
+++ R/trakt.show.stats.R
@@ -5,22 +5,28 @@
 #' 
 #' \code{trakt.show.stats} pulls show stats and returns it compactly.
 #' The data includes show ratings, scrobbles, checkins, plays, comments…
-#' @param target The \code{slug} or \code{tvdbid} of the show requested
-#' @param apikey API-key used for the call. Defaults to \code{getOption("trakt.apikey")}
+#' @param target The \code{slug} of the show requested
+#' @param extended Whether extended info should be provided. 
+#' Defaults to \code{"full"}, can either be \code{"min"} or \code{"full"}
 #' @return A \code{list} containing show stats
 #' @export
-#' @note See \href{http://trakt.tv/api-docs/show-stats}{the trakt API docs for further info}
+#' @note See \href{http://docs.trakt.apiary.io/#reference/shows/stats}{the trakt API docs for further info}
 #' @examples
 #' \dontrun{
-#' options(trakt.apikey = jsonlite::fromJSON("key.json")$apikey)
+#' get_trakt_credentials() # Set required API data/headers
 #' breakingbad.stats <- trakt.show.stats("breaking-bad")
 #' }
-trakt.show.stats <- function(target, apikey = getOption("trakt.apikey")){
-  if (is.null(apikey)){
-    stop("No API key set")
+trakt.show.stats <- function(target){
+  if (is.null(getOption("trakt.headers"))){
+    stop("HTTP headers not set, see ?get_trakt_credentials")
   }
-  baseURL  <- "http://api.trakt.tv/show/stats.json/"
-  url      <- paste0(baseURL, apikey, "/", target)
-  response <- jsonlite::fromJSON(url)
+  
+  # Constructing URL
+  baseURL <- "https://api-v2launch.trakt.tv/shows"
+  url     <- paste0(baseURL, "/", target, "/stats")
+  url     <- paste0(url, "?extended=", extended)
+  
+  # Actual API call
+  response <- trakt.api.call(url = url)
   return(response)
 }
