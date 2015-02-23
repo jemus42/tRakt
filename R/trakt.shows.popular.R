5cd7c0c91d160f3348659e0d3dca068c6b8659a0
jemus42 <jemus42@quantenbrot.de>
2015-02-24 00:32:32 +0100

    Add extended option

--- R/trakt.shows.popular.R
+++ R/trakt.shows.popular.R
@@ -3,6 +3,8 @@
 #' \code{trakt.shows.popular} returns a list of the most popular shows on trakt.tv.
 #' According to the API docs, opularity is calculated based both ratings and number of ratings.
 #' @param limit Number of shows to return. Is coerced to \code{integer} and must be greater than 0.
+#' @param extended Whether extended info should be provided.
+#' Defaults to \code{"full"}, can either be \code{"min"} or \code{"full"}
 #' @return A \code{data.frame} containing popular shows with their name and ids
 #' @export
 #' @note See \href{http://docs.trakt.apiary.io/#reference/shows/get-popular-shows}{the trakt API docs for further info}
@@ -12,7 +14,7 @@
 #' get_trakt_credentials() # Set required API data/headers
 #' trakt.shows.popular(5)
 #' }
-trakt.shows.popular <- function(limit = 10){
+trakt.shows.popular <- function(limit = 10, extended = "min"){
   limit <- as.integer(limit)
   if (limit < 1){
     stop("Limit must be greater than zero")
@@ -20,11 +22,16 @@ trakt.shows.popular <- function(limit = 10){
   ids <- NULL
 
   baseURL  <- "https://api-v2launch.trakt.tv/shows/popular"
-  url      <- paste0(baseURL, "?page=1&limit=", limit)
+  url      <- paste0(baseURL, "?page=1&limit=", limit, "&extended=", extended)
   response <- trakt.api.call(url)
 
   # Spreading out ids to get a flat data.frame
   response <- cbind(subset(response, select = -ids), response$ids)
 
+  # Handling date
+  response$first_aired <- lubridate::parse_date_time(response$first_aired,
+                                                     "%y-%m-%dT%H-%M-%S", truncated = 3)
+  response$updated_at <- lubridate::parse_date_time(response$updated_at,
+                                                    "%y-%m-%dT%H-%M-%S", truncated = 3)
   return(response)
 }
--- R/trakt.shows.trending.R
+++ R/trakt.shows.trending.R
@@ -4,6 +4,8 @@
 #' According to the API docs, it returns all shows being watched right now, where
 #' shows with the most users are returned first.
 #' @param limit Number of shows to return. Is coerced to \code{integer} and must be greater than 0.
+#' @param extended Whether extended info should be provided.
+#' Defaults to \code{"full"}, can either be \code{"min"} or \code{"full"}
 #' @return A \code{data.frame} containing trending shows with their name and ids
 #' @export
 #' @note See \href{http://docs.trakt.apiary.io/#reference/shows/get-trending-shows}{the trakt API docs for further info}
@@ -13,7 +15,7 @@
 #' get_trakt_credentials() # Set required API data/headers
 #' trakt.shows.trending(5)
 #' }
-trakt.shows.trending <- function(limit = 10){
+trakt.shows.trending <- function(limit = 10, extended = "min"){
   limit <- as.integer(limit)
   if (limit < 1){
     stop("Limit must be greater than zero")
@@ -21,12 +23,17 @@ trakt.shows.trending <- function(limit = 10){
   ids <- NULL; show <- NULL
 
   baseURL  <- "https://api-v2launch.trakt.tv/shows/trending"
-  url      <- paste0(baseURL, "?page=1&limit=", limit)
+  url      <- paste0(baseURL, "?page=1&limit=", limit, "&extended=", extended)
   response <- trakt.api.call(url)
 
   # Spreading out ids to get a flat data.frame
   response$show <- cbind(subset(response$show, select = -ids), response$show$ids)
   response      <- cbind(subset(response, select = -show), response$show)
 
+  # Handling date
+  response$first_aired <- lubridate::parse_date_time(response$first_aired,
+                                                     "%y-%m-%dT%H-%M-%S", truncated = 3)
+  response$updated_at <- lubridate::parse_date_time(response$updated_at,
+                                                    "%y-%m-%dT%H-%M-%S", truncated = 3)
   return(response)
 }
