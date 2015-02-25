ce72a03dfa1016c75217a690af17fbc0717f9eec
jemus42 <jemus42@quantenbrot.de>
2015-02-25 14:22:18 +0100

    Add movie data tests

---
+++ tests/testthat/test_movie-data.R
@@ -0,0 +1,18 @@
+context("Movie functions")
+
+test_that("trakt.movie.people returns a list of data.frames", {
+  people <- trakt.movie.people("tron-legacy-2010")
+  expect_is(people, "list")
+  expect_is(people$cast, "data.frame")
+  expect_is(people$crew, "list")
+})
+
+test_that("trakt.movie.ratings returns data.frame", {
+  expect_is(trakt.movie.ratings(target = "tron-legacy-2010"), "list")
+  expect_is(trakt.movie.ratings(target = "tron-legacy-2010")$distribution, "data.frame")
+})
+
+test_that("trakt.movie.summary returns list", {
+  expect_is(trakt.movie.summary(target = "tron-legacy-2010", extended = "min"), "list")
+  expect_is(trakt.movie.summary(target = "tron-legacy-2010", extended = "full"), "list")
+})
