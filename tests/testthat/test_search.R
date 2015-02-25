36352b8192ce4b9371a7a8d8106573374a29d472
jemus42 <jemus42@quantenbrot.de>
2015-02-25 21:31:07 +0100

    Expand search function testing

--- tests/testthat/test_search.R
+++ tests/testthat/test_search.R
@@ -1,5 +1,10 @@
 context("Search functions")
 
+test_that("search returns list for both show and movie query", {
+  expect_is(trakt.search("Breaking Bad", type = "show"), "data.frame")
+  expect_is(trakt.search("TRON", type = "movie"), "data.frame")
+})
+
 test_that("search by keyword and search by id return identical results", {
   expect_identical(trakt.search("Breaking Bad"), trakt.search.byid("tt0903747", id_type = "imdb"))
 })
