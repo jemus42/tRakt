26d92b10bb9d7e12b68f7f5c882ccbe8f3b445d0
jemus42 <jemus42@quantenbrot.de>
2015-02-25 14:14:26 +0100

    Add show summary function tests

--- tests/testthat/test_show-data.R
+++ tests/testthat/test_show-data.R
@@ -16,7 +16,34 @@ test_that("trakt.show.season returns data.frame", {
   expect_is(trakt.show.season(target = "game-of-thrones", 2, extended = "full"), "data.frame")
 })
 
-test_that("trakt.show.summary returns data.frame", {
+test_that("trakt.show.summary returns list", {
   expect_is(trakt.show.summary(target = "game-of-thrones", extended = "min"), "list")
   expect_is(trakt.show.summary(target = "game-of-thrones", extended = "full"), "list")
+})
+
+#### Test the summary functions ####
+context("Show summary functions")
+
+test_that("trakt.getSeasons returns data.frame", {
+  expect_is(trakt.getSeasons(target = "game-of-thrones", extended = "min"), "data.frame")
+  expect_is(trakt.getSeasons(target = "game-of-thrones", extended = "full"), "data.frame")
+  expect_is(trakt.getSeasons(target = "game-of-thrones", extended = "full,images"), "data.frame")
+})
+
+test_that("trakt.getEpisodeData returns data.frame", {
+  expect_is(trakt.getEpisodeData(target = "game-of-thrones", c(1,2,3),
+                                 extended = "min"), "data.frame")
+  expect_is(trakt.getEpisodeData(target = "game-of-thrones", c(1,2,3),
+                                 extended = "full"), "data.frame")
+  expect_is(trakt.getEpisodeData(target = "game-of-thrones", c(1,2,3),
+                                 extended = "full,images"), "data.frame")
+})
+
+test_that("trakt.getFullShowData returns properly structured list", {
+  show <- trakt.getFullShowData("Breaking Bad")
+  expect_is(show, "list")
+  expect_is(show$info, "data.frame")
+  expect_is(show$summary, "list")
+  expect_is(show$seasons, "data.frame")
+  expect_is(show$episodes, "data.frame")
 })
