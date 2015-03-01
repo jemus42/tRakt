d19232cc4f883afc3f8d11d6774ebd1bf0e1722e
jemus42 <jemus42@quantenbrot.de>
2015-03-02 00:10:46 +0100

    Take a closer look at that fixed test and fix it again

--- tests/testthat/test_people-data.R
+++ tests/testthat/test_people-data.R
@@ -7,7 +7,7 @@ test_that("trakt.people.summary returns data.frame", {
 
 test_that("trakt.people.movies returns data.frame", {
   people.movies.min <- trakt.people.movies(target = "bryan-cranston", extended = "min")
-  expect_is(people.movies, "list")
+  expect_is(people.movies.min, "list")
   expect_is(people.movies.min$cast, "data.frame")
 
   people.movies.full <- trakt.people.movies(target = "bryan-cranston", extended = "full")
@@ -18,7 +18,7 @@ test_that("trakt.people.movies returns data.frame", {
 test_that("trakt.people.shows returns data.frame", {
   people.shows.min <- trakt.people.shows(target = "bryan-cranston", extended = "min")
   expect_is(people.shows.min, "list")
-  expect_is(people.movies.min$cast, "data.frame")
+  expect_is(people.shows.min$cast, "data.frame")
 
   people.shows.full <- trakt.people.shows(target = "bryan-cranston", extended = "full")
   expect_is(people.shows.full, "list")
