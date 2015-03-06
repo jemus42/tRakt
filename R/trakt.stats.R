88237e697db3daca03debd48e3d1e9259c0eee87
jemus42 <jemus42@quantenbrot.de>
2015-03-06 02:26:04 +0100

    Generalize (still defunct) stats function

--- R/trakt.stats.R
+++ R/trakt.stats.R
@@ -1,6 +1,6 @@
-#' [Defunct] Get a show's summary info
+#' [Defunct] Get a show or movie's summary info
 #'
-#' DEFUNCT as of 2015-02-11,
+#' DEFUNCT as of 2015-03-06,
 #' see \href{http://docs.trakt.apiary.io/reference/shows/stats/get-show-stats}{their API docs}
 #'
 #' \code{trakt.show.stats} pulls show stats and returns it compactly.
@@ -12,16 +12,17 @@
 #' @return A \code{list} containing show stats
 #' @export
 #' @note See \href{http://docs.trakt.apiary.io/#reference/shows/stats}{the trakt API docs for further info}
-#' @family show
+#' @family show data
+#' @family movie data
 #' @examples
 #' \dontrun{
 #' get_trakt_credentials() # Set required API data/headers
-#' breakingbad.stats <- trakt.show.stats("breaking-bad")
+#' breakingbad.stats <- trakt.stats(type = "shows", "breaking-bad")
 #' }
-trakt.show.stats <- function(target, extended = "min"){
+trakt.stats <- function(target, type = "shows", extended = "min"){
 
   # Construct URL, make API call
-  url      <- build_trakt_url("shows", target, "stats", extended = extended)
+  url      <- build_trakt_url(type, target, "stats", extended = extended)
   response <- trakt.api.call(url = url)
 
   return(response)
