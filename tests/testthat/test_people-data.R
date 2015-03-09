42cd187190e390a11a772e0020ffa4f3278e381d
jemus42 <jemus42@quantenbrot.de>
2015-03-09 10:27:10 +0100

    Substitute Bryan Cranston with Peter Dinklage, as you do.

--- tests/testthat/test_people-data.R
+++ tests/testthat/test_people-data.R
@@ -10,21 +10,21 @@ test_that("trakt.people.summary returns data.frame", {
 })
 
 test_that("trakt.people.movies returns data.frame", {
-  people.movies.min <- trakt.people.movies(target = "bryan-cranston", extended = "min")
+  people.movies.min <- trakt.people.movies(target = sample_person, extended = "min")
   expect_is(people.movies.min, "list")
   expect_is(people.movies.min$cast, "data.frame")
 
-  people.movies.full <- trakt.people.movies(target = "bryan-cranston", extended = "full")
+  people.movies.full <- trakt.people.movies(target = sample_person, extended = "full")
   expect_is(people.movies.full, "list")
   expect_is(people.movies.full$cast, "data.frame")
 })
 
 test_that("trakt.people.shows returns data.frame", {
-  people.shows.min <- trakt.people.shows(target = "bryan-cranston", extended = "min")
+  people.shows.min <- trakt.people.shows(target = sample_person, extended = "min")
   expect_is(people.shows.min, "list")
   expect_is(people.shows.min$cast, "data.frame")
 
-  people.shows.full <- trakt.people.shows(target = "bryan-cranston", extended = "full")
+  people.shows.full <- trakt.people.shows(target = sample_person, extended = "full")
   expect_is(people.shows.full, "list")
   expect_is(people.shows.full$cast, "data.frame")
 })
