b33da6aaf2819b7826e37a379c1b0a0f53dc8922
Lukas Burk <lukas@quantenbrot.de>
2019-02-19 21:48:53 +0100

    Nuke old tests, start new ones

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
 library(tRakt)
 
-# test_check("tRakt")
+test_check("tRakt")
---
+++ tests/testthat/test-api.R
@@ -0,0 +1,28 @@
+context("test-test-api")
+
+test_that("Client ID is set without .Renviron", {
+  Sys.setenv(trakt_client_id = "")
+  expect_message(get_trakt_credentials())
+  expect_equal(
+    getOption("trakt.client.id"),
+    "12fc1de7671c7f2fb4a8ac08ba7c9f45b447f4d5bad5e11e3490823d629afdf2"
+  )
+  expect_message(get_trakt_credentials(silent = TRUE))
+  expect_is(getOption("trakt.headers"), "request")
+
+  get_trakt_credentials(username = "arbitraryusername")
+  expect_equal(getOption("trakt.username"), "arbitraryusername")
+})
+
+test_that("trakt.api.call can make API calls", {
+  skip_on_cran()
+
+  url <- "https://api-v2launch.trakt.tv/shows/breaking-bad?extended=min"
+  result <- trakt.api.call(url)
+
+  expect_is(result, "list")
+  expect_error(trakt.api.call("https://example.com"))
+
+  options("trakt.headers" = NULL)
+  expect_error(trakt.api.call(url, headers = NULL))
+})
