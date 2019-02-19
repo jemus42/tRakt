7fff9cf790d273d1f52ed993c0d6c4858cd6d624
Lukas Burk <lukas@quantenbrot.de>
2019-02-19 17:48:11 +0100

    Switch from key.json to .Renviron variables for API key

--- R/api.R
+++ R/api.R
@@ -3,89 +3,62 @@
 #' `get_trakt_credentials` searches for your credentials and stores them
 #' in the appropriate `option` variables.
 #' It also sets the HTTP header required for v2 API calls.
-#' To make this work automatically, place a `key.json` file either in the working directory
-#' or in `~/.config/trakt/key.json`.
+#' To make this work automatically, place your key as environment variables in
+#' `~/.Renviron`.
 #' Arguments to this function take precedence over any key file.
 #'
-#' @param username Explicitly set your trakt.tv username (Not used yet)
-#' @param client.id Explicitly set your APIv2 client id
-#' @param client.secret Explicitly set your APIv2 client secret
-#' @param set.headers `TRUE` by default. Sets the `httr` headers
-#' for `GET` requests for the APIv2
-#' @param silent If TRUE (default), no messages are printed showing you the API information.
+#' @param username `character(1)`: Explicitly set your trakt.tv username (optional).
+#' @param client.id `character(1)`: Explicitly set your APIv2 client id (required for API interaciton).
+#' @param set.headers `logical(1) [TRUE]`: Sets the `httr` headers
+#' for `GET` requests to the APIv2.
+#' @param silent `logical(1) [TRUE]`: No messages are printed showing you the API information.
 #' Mostly for debug purposes.
 #' @return Nothing. Only messages.
 #' @export
-#' @importFrom jsonlite fromJSON
-#' @note Please note that no oauth2 methods are supported yet,
+#' @note Please note that no OAuth2 methods are supported yet,
 #' only client id really matters.
 #' @family API-basics
 #' @examples
 #' \dontrun{
-#' # Use a key.json
+#' # Set appropriate values in your .Renviron like this:
+#' (e.g. in ~/.Renviron, depending on your platform)
+#'
+#' # tRakt
+#' trakt_username=jemus42
+#' trakt_client_id=12fc1de7671c7f2fb4a8ac08ba7c9f45b447f4d5bad5e11e3490823d629afdf2
+#'
+#' # Use a values set in ~/.Renviron in an R session:
+#' # (This is automatically executed when calling library(tRakt))
 #' get_trakt_credentials()
-#' 
-#' # Explicitly set values
+#'
+#' # Explicitly set values in an R session, overriding .Renviron values f present
 #' get_trakt_credentials(
 #'   username = "sean",
 #'   client.id = "12fc1de7671c7f2fb4a8ac08ba7c9f45b447f4d5bad5e11e3490823d629afdf2"
 #' )
 #' }
-get_trakt_credentials <- function(username = NULL, client.id = NULL,
-                                  client.secret = NULL, set.headers = TRUE,
+get_trakt_credentials <- function(username = "", client.id = "",
+                                  set.headers = TRUE,
                                   silent = TRUE) {
-  # Finding/setting key file
-  if (file.exists("~/.config/trakt/key.json")) {
-    keyfile <- "~/.config/trakt/key.json"
-  } else if (file.exists("key.json")) {
-    keyfile <- "./key.json"
-  } else {
-    keyfile <- NULL
-  }
-  if (!(is.null(keyfile))) {
-    if (!silent) message(paste("Reading credentials from", keyfile))
-  } else {
-    if (!silent) message("No keyfile set/found")
-  }
 
-  # Setting username (just in case)
-  if (!is.null(username)) {
+  username  <- ifelse(username == "", Sys.getenv("trakt_username"), username)
+  client_id <- ifelse(client.id == "", Sys.getenv("trakt_client_id"), client.id)
+
+  if (username != "") {
     options(trakt.username = username)
-  } else if (!(is.null(keyfile))) {
-    options(trakt.username = jsonlite::fromJSON(keyfile)[["username"]])
-  } else {
-    if (!silent) warning("Couldn't find your username")
+    if (!silent) {
+      message(paste("Your trakt.tv username is set to", getOption("trakt.username")))
+    }
   }
-
-  # Setting v2 client id
-  if (!(is.null(client.id))) {
-    options(trakt.client.id = client.id)
-  } else if (!(is.null(keyfile))) {
-    options(trakt.client.id = jsonlite::fromJSON(keyfile)[["client.id"]])
+  if (client_id != "") {
+    options(trakt.client.id = client_id)
   } else {
-    if (!silent) warning("Couldn't find your client id")
-  }
-
-  # Setting v2 client secret (not used yet)
-  if (!(is.null(client.secret))) {
-    options(trakt.client.secret = client.id)
-  } else if (!(is.null(keyfile))) {
-    options(trakt.client.secret = jsonlite::fromJSON(keyfile)[["client.secret"]])
-  } else {
-    if (!silent) warning("Couldn't find your client secret")
+    options(trakt.client.id = "12f[...]df2")
+    message("I provided my client.id as a fallback for you. Please use it responsibly.")
   }
 
   if (!silent) {
-    # Communicate the above
-    message("Please check if everything seems right:")
-    message(paste("Your trakt.tv username is set to", getOption("trakt.username")))
     message(paste("Your APIv2 client id is set to", getOption("trakt.client.id")))
-    message("Your APIv2 client secret is set (not displayed for privacy reasons)")
-  }
-
-  if (is.null(getOption("trakt.client.id"))) {
-    options(trakt.client.id = "12fc1de7671c7f2fb4a8ac08ba7c9f45b447f4d5bad5e11e3490823d629afdf2")
-    warning("I provided my client.id as a fallback for you. Use it responsibly.")
   }
 
   # Set the appropriate header for httr::GET
@@ -122,7 +95,7 @@ get_trakt_credentials <- function(username = NULL, client.id = NULL,
 #' @family API-basics
 #' @examples
 #' \dontrun{
-#' get_trakt_credentials() # Set required API data/headers
+#' library(tRakt)
 #' trakt.api.call("https://api-v2launch.trakt.tv/shows/breaking-bad?extended=min")
 #' }
 trakt.api.call <- function(url, headers = getOption("trakt.headers"), fromJSONify = TRUE,
