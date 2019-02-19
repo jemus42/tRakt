d7f6b37e9284366e494f7f6d4c4ac86e040b134c
Lukas Burk <lukas@quantenbrot.de>
2019-02-20 00:25:51 +0100

    Also test trakt.get_full_showdata

--- tests/testthat/test-trakt-get_all_episodes.R
+++ tests/testthat/test-trakt-get_all_episodes.R
@@ -1,6 +1,6 @@
-context("test-trakt-get_all_episodes")
+context("test-trakt-get_a_lot")
 
-test_that("it works", {
+test_that("trakt.get_all_episodes works", {
   skip_on_cran()
   result <- trakt.get_all_episodes(c("futurama", "breaking-bad"))
 
@@ -8,4 +8,14 @@ test_that("it works", {
   expect_true(nrow(result) > 120)
   expect_true(ncol(result) > 6)
   expect_equal(unique(result$show), c("futurama", "breaking-bad"))
+})
+
+test_that("trakt.get_full_showdata works", {
+  skip_on_cran()
+  result <- trakt.get_full_showdata("Breaking Bad")
+
+  expect_is(result, "list")
+  expect_named(result, c("info", "summary", "seasons", "episodes"))
+  expect_true(nrow(result$episodes) > 40)
+  expect_true(ncol(result$episodes) > 6)
 })
