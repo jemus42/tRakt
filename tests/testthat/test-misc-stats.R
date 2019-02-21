c2f4874206cb6da797f76be0a5146fdbb0a85170
Lukas Burk <lukas@quantenbrot.de>
2019-02-21 22:07:02 +0100

    Fix test?

--- tests/testthat/test-misc-stats.R
+++ tests/testthat/test-misc-stats.R
@@ -1,6 +1,8 @@
 context("test-misc-stats")
 
 test_that("trakt.user.stats works", {
+  skip_on_cran()
+  skip_if_not(class(getOption("trakt.headers")) == "request")
   user <- "jemus42"
 
   userstats <- trakt.user.stats(user = user)
