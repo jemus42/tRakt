00873a79b225ac87c59cb8d0116607f49f52ad2e
Lukas Burk <lukas@quantenbrot.de>
2019-02-22 21:46:05 +0100

    Fix up pkg doc

--- R/tRakt-package.R
+++ R/tRakt-package.R
@@ -1,10 +1,3 @@
-#' Pull Data from trakt.tv
-#'
-#' tRakt is a package that grew out of my initial attempt to compare Stargate SG-1 episode ratings.
-#' \href{http://stuff.wurstmannberg.de/tRakt}{Here are some of the resulting graphs}.
-#' This package is meant to provide the basic functions required to pull series data, which
-#' then can be used either for interactive use and standalone graphs, or for more complex applications.
-#' @name tRakt
 #' @importFrom tibble tibble
-#' @docType package
-NULL
+#' @keywords internal
+"_PACKAGE"
