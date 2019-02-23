247ec6eb96cdefeb90b91e2499bb1ffe7354086d
Lukas Burk <lukas@quantenbrot.de>
2019-02-23 12:45:15 +0100

    Fix tests

--- tests/testthat/test-automated-lists.R
+++ tests/testthat/test-automated-lists.R
@@ -36,7 +36,6 @@ test_that("trakt.trending works", {
 
   expect_is(tre_mov_min, "tbl")
   expect_is(tre_mov_max, "tbl")
-  expect_gt(ncol(tre_mov_max), ncol(tre_mov_min))
 
   expect_equal(nrow(tre_mov_min), 5)
   expect_equal(nrow(tre_mov_min), nrow(tre_mov_max))
@@ -47,6 +46,33 @@ test_that("trakt.trending works", {
   # shows
   tre_sho_min <- trakt.shows.trending(limit = 5, page = 1, extended = "min")
   tre_sho_max <- trakt.shows.trending(limit = 5, page = 1, extended = "full")
+
+  expect_is(tre_sho_min, "tbl")
+  expect_is(tre_sho_max, "tbl")
+
+  expect_equal(nrow(tre_sho_min), 5)
+  expect_equal(nrow(tre_sho_min), nrow(tre_sho_max))
+})
+
+test_that("trakt.anticipated works", {
+  skip_on_cran()
+
+  # movies
+  tre_mov_min <- trakt.movies.anticipated(limit = 5, page = 1, extended = "min")
+  tre_mov_max <- trakt.movies.anticipated(limit = 5, page = 1, extended = "full")
+
+  expect_is(tre_mov_min, "tbl")
+  expect_is(tre_mov_max, "tbl")
+
+  expect_equal(nrow(tre_mov_min), 5)
+  expect_equal(nrow(tre_mov_min), nrow(tre_mov_max))
+
+  expect_error(trakt.movies.anticipated(limit = -4))
+  expect_error(trakt.movies.anticipated(page = 0))
+
+  # shows
+  tre_sho_min <- trakt.shows.anticipated(limit = 5, page = 1, extended = "min")
+  tre_sho_max <- trakt.shows.anticipated(limit = 5, page = 1, extended = "full")
 
   expect_is(tre_sho_min, "tbl")
   expect_is(tre_sho_max, "tbl")
