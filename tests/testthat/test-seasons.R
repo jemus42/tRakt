2caa69e1b84bec52030a7517145fe1ee0c94150f
Lukas Burk <lukas@quantenbrot.de>
2019-02-24 22:05:25 +0100

    Fix tests

--- tests/testthat/test-seasons.R
+++ tests/testthat/test-seasons.R
@@ -6,7 +6,7 @@ test_that("trakt.seasons.season works", {
   target <- "futurama"
 
   min_names <- c(
-    "season_number", "episode", "title", "trakt", "tvdb",
+    "season", "episode_number", "title", "trakt", "tvdb",
     "imdb", "tmdb", "tvrage"
   )
 
--- tests/testthat/test-trakt_summary.R
+++ tests/testthat/test-trakt_summary.R
@@ -1,61 +1,38 @@
-context("test-trakt_summary")
+context("test-media-summary")
 
-test_that("trakt.show.summary works", {
+test_that("trakt.shows.summary works", {
   skip_on_cran()
-  
+
   target <- "breaking-bad"
-  show_min_list <- trakt.show.summary(target, force_data_frame = FALSE)
-  show_min_df <- trakt.show.summary(target, force_data_frame = TRUE)
+  show_min_df <- trakt.shows.summary(target)
+  show_full_df <- trakt.shows.summary(target, extended = "full")
 
-  show_full_list <- trakt.show.summary(target,
-    extended = "full",
-    force_data_frame = FALSE
-  )
-  show_full_df <- trakt.show.summary(target,
-    extended = "full",
-    force_data_frame = TRUE
-  )
-
-  expect_is(show_min_list, "list")
   expect_is(show_min_df, "tbl")
-  expect_is(show_full_list, "list")
   expect_is(show_full_df, "tbl")
 
-  expect_true(length(show_min_list) < length(show_full_list))
   expect_true(length(show_min_df) < length(show_full_df))
 
   expect_identical(
-    trakt.show.summary(c(target, target)),
+    trakt.shows.summary(c(target, target)),
     rbind(show_min_df, show_min_df)
   )
 })
 
-test_that("trakt.movie.summary works", {
+test_that("trakt.movies.summary works", {
   skip_on_cran()
-  
+
   target <- "deadpool-2016"
-  movie_min_list <- trakt.movie.summary(target, force_data_frame = FALSE)
-  movie_min_df <- trakt.movie.summary(target, force_data_frame = TRUE)
+  movie_min_df <- trakt.movies.summary(target)
 
-  movie_full_list <- trakt.movie.summary(target,
-    extended = "full",
-    force_data_frame = FALSE
-  )
-  movie_full_df <- trakt.movie.summary(target,
-    extended = "full",
-    force_data_frame = TRUE
-  )
+  movie_full_df <- trakt.movies.summary(target, extended = "full")
 
-  expect_is(movie_min_list, "list")
   expect_is(movie_min_df, "tbl")
-  expect_is(movie_full_list, "list")
   expect_is(movie_full_df, "tbl")
 
-  expect_true(length(movie_min_list) < length(movie_full_list))
   expect_true(length(movie_min_df) < length(movie_full_df))
 
   expect_identical(
-    trakt.movie.summary(c(target, target)),
+    trakt.movies.summary(c(target, target)),
     rbind(movie_min_df, movie_min_df)
   )
 })
