75aae0095c8452b8d1968d2ea0c1d10995c46ebf
Lukas Burk <lukas@quantenbrot.de>
2019-02-26 04:44:54 +0100

    Fix up search stuff
    Still needs deduplication

--- R/trakt.search.R
+++ R/trakt.search.R
@@ -1,21 +1,24 @@
-#' Search for a show via text query
+#' Search for a show via text query or id
 #'
-#' `trakt.search` returns search result info.
-#'
-#' Search for a show or movie with a keyword (e.g. `"Breaking Bad"`) and receive basic info of the first
-#' search result. It's main use is to retrieve the ids or proper show/movie title for further use, as well
-#' as receiving a quick overview of a show/movie.
+#' Search for a show or movie with a keyword (e.g. `"Breaking Bad"`) and receive
+#' basic info of the first search result. It's main use is to retrieve
+#' the ids or proper show/movie title for further use, as well
+#' as receiving a quick overview of a show/movie. The amount of
+#' information returned is equal to `.summary` API methods and also depends on
+#' the value of `extended`.
 #' @param query The keyword used for the search.
+#' @param id The id used for the search.
+#' @param id_type The type of `id`. One of `trakt` (default), `imdb`, `tmdb`, `tvdb`.
 #' @param type `character(1) ["movie"]`: The type of data you're looking for. One of `show`,
 #' `movie`, `episode`, `person` or `list` or a character vector with those elements, e.g.
 #' `c("show", "movie")`.
 #' @param years Optionally filter by years.
 #' @param n_results `integer(1) [1]`: How many results to return.
-#' @return A [tibble][tibble::tibble-package] containing a single search result.
+#' @return A [tibble][tibble::tibble-package] containing a `n_result` result.
 #'         If no results are found, the `tibble` has 0 rows.
 #' @inherit trakt_api_common_parameters
 #' @export
-#' @note The API technically allows concatenated types,
+#' @note The API technically allows concatenated types for text query searches,
 #' e.g. `movie,show,episode` to search all three types of items, but this is not supported
 #' in this package. If you really want this functionality, open an issue on GitHub.
 #' @source [The trakt.tv API docs](https://trakt.docs.apiary.io/#reference/search/text-query/get-text-query-results)
@@ -24,10 +27,16 @@
 #' @importFrom utils head
 #' @examples
 #' \dontrun{
+#' # A show
 #' trakt.search("Breaking Bad", type = "show", n_results = 3)
 #'
+#' # A show by its trakt id
+#' trakt.search.byid(1388, "trakt", type = "show")
+#'
+#' # A person
 #' trakt.search("J. K. Simmons", type = "person", extended = "full")
 #'
+#' # A movie or a show
 #' trakt.search("Tron", "movie", n_results = 2)
 #' trakt.search("Tron", "show", n_results = 2)
 #' }
@@ -75,28 +84,14 @@ trakt.search <- function(query, type = c("movie", "show", "episode", "person", "
   response
 }
 
-#' Lookup a show via id
-#'
-#' `trakt.search.byid` retrieves show stats and returns it compactly.
-#'
-#' @param id The id used for the search.
-#' @param id_type The type of `id`. Defaults to `trakt-show`, can be
-#' `trakt-movie`, `trakt-show`, `trakt-episode`, `imdb`,
-#' `tmdb`, `tvdb`, `tvrage`
-#' @return A [tibble][tibble::tibble-package] containing a single search result.
-#'         If no results are found, the `tibble` has 0 rows.
+#' @rdname trakt.search
 #' @export
-#' @note See \href{http://docs.trakt.apiary.io/reference/search/id-lookup/get-id-lookup-results}{the trakt API docs for further info}
-#' @family API-basics
-#' @family search functions
-#' @examples
-#' \dontrun{
-#' breakingbad <- trakt.search.byid(1388, "trakt-show")
-#' }
-trakt.search.byid <- function(id, id_type = "trakt-show") {
+trakt.search.byid <- function(id, id_type = "trakt",
+                              type = c("movie", "show", "episode", "person", "list"),
+                              n_results = 1L, extended = c("min", "full")) {
 
   # Construct URL, make API call
-  url <- build_trakt_url("search", id_type = id_type, id = id)
+  url <- build_trakt_url("search", id_type, id, type = type)
   response <- trakt.api.call(url = url)
 
   # Check if response is empty (nothing found)
@@ -105,9 +100,23 @@ trakt.search.byid <- function(id, id_type = "trakt-show") {
     return(tibble::tibble())
   }
 
-  response <- response[[ncol(response)]]
-  response <- response[names(response) != "images"]
-  response <- cbind(response[names(response) != "ids"], response$ids)
+  if (type == "show" & extended == "full") {
+    response$show <- unpack_show(response$show)
+  }
 
-  tibble::as_tibble(response)
+  response <- cbind(response[names(response) != type], response[[type]])
+
+  # Already handled by unpack_show so it would fail here
+  if (tibble::has_name(response, "ids")) {
+    response <- cbind(response[names(response) != "ids"], fix_ids(response$ids))
+  }
+
+  # Sanity filter: Remove items with a year of NA, like "show" + "tron"
+  # Happens for exact title matches (bumps "score") but e.g. false type in this case
+  response <- tibble::as_tibble(response)
+  if (tibble::has_name(response, "year")) {
+    response <- response[!(is.na(response$year) & response$score == 1000), ]
+  }
+  response <- utils::head(response, n_results)
+  response
 }
