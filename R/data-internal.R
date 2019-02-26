73a89515d72e0a5340e9718e263932f47452f38d
Lukas Burk <lukas@quantenbrot.de>
2019-02-26 02:11:08 +0100

    Examples for datasets

--- R/data-internal.R
+++ R/data-internal.R
@@ -1,16 +1,26 @@
 #' Genres
+#' @examples
+#' genres
 "genres"
 
 #' Language codes
+#' @examples
+#' languages
 "languages"
 
 #' Available networks
+#' @examples
+#' networks
 "networks"
 
 #' Country codes
+#' @examples
+#' countries
 "countries"
 
 #' Certifications
 #'
 #' Currently only US certification are available.
+#' @examples
+#' certiciations
 "certifications"
