9f2192e61961570aa12db0af6152d2a098a8c08e
Lukas Burk <lukas@quantenbrot.de>
2019-03-02 03:06:51 +0100

    Fix up trakt.user.collection

--- R/trakt.user.collection.R
+++ R/trakt.user.collection.R
@@ -3,34 +3,53 @@
 #' `trakt.user.collection` retrieves a user's collected shows or movies.
 #' It does not use OAuth2, so you can only get data for a user with a
 #' public profile.
+#'
+#' @details
+#' This function wraps [this API method](https://trakt.docs.apiary.io/#reference/users/collection/get-collection)
+#' at the endpoint `/users/:user_id/collection/:type`.
+#' @note The `extended = "metadata"` API parameter is not implemented. This would
+#' add media information `media_type`, `resolution`, `audio`, `audio_channels` and `3D`
+#' to the output, which may or may not be available. If this feature is important to
+#' you, please open an issue on GitHub.
+#'
 #' @inheritParams trakt_api_common_parameters
 #' @param unnest_episodes `logical(1) [FALSE]`: Unnests episode data using
 #' `[tidyr](tidyr::unnest)` and returns one row per episode rather than one row per show.
 #' @return A [tibble][tibble::tibble-package].
 #' @export
 #' @family user data
-#' @import dplyr
 #' @importFrom lubridate ymd_hms
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom dplyr bind_cols
+#' @importFrom dplyr everything
+#' @importFrom dplyr rename
+#' @importFrom tibble as_tibble
+#' @importFrom purrr map
 #' @examples
 #' \dontrun{
 #' seans.movies <- trakt.user.collection(user = "sean", type = "movies")
 #' }
 trakt.user.collection <- function(user = getOption("trakt.username"),
                                   type = c("shows", "movies"),
+                                  extended = c("min", "full"),
                                   unnest_episodes = FALSE) {
   check_username(user)
   type <- match.arg(type)
+  extended <- match.arg(extended)
 
   # Construct URL, make API call
-  url <- build_trakt_url("users", user, "collection", type)
+  url <- build_trakt_url("users", user, "collection", type, extended = extended)
   response <- trakt.api.call(url = url)
 
   if (type == "shows") {
-    # Flatten out ids
-    response$show <- cbind(response$show[names(response$show) != "ids"], fix_ids(response$show$ids))
-    names(response$show) <- paste0("show_", names(response$show))
-    response <- cbind(response[names(response) != "show"], response$show)
-    response <- dplyr::select(response, -seasons, dplyr::everything(), seasons)
+
+    response <- response %>%
+      select(-show) %>%
+      bind_cols(unpack_show(response$show)) %>%
+      as_tibble() %>%
+      select(-seasons, everything(), seasons) %>%
+      mutate(seasons = map(seasons, as_tibble))
 
     # I think importing tidyr for this alone is worth it, because
     # A list structure this big and deeply nested is just wrong:
@@ -41,23 +60,18 @@ trakt.user.collection <- function(user = getOption("trakt.username"),
         stop("This functionality requires the tidyr package")
       }
 
-      response <- tibble::as_tibble(response) %>%
+      response <- as_tibble(response) %>%
         tidyr::unnest(seasons) %>%
-        dplyr::rename(season = number) %>%
+        rename(season = number) %>%
         tidyr::unnest(episodes) %>%
-        dplyr::rename(episode = number) %>%
-        dplyr::mutate(collected_at = lubridate::ymd_hms(collected_at))
+        rename(episode = number) %>%
+        dplyr::mutate(collected_at = ymd_hms(collected_at))
     }
   } else if (type == "movies") {
-    # Flatten out ids
-    response$movie <- cbind(
-      response$movie[names(response$movie) != "ids"],
-      fix_ids(response$movie$ids)
-    )
-    response <- cbind(response[names(response) != "movie"], response$movie)
+    response <- unpack_movie(response)
   }
   # To be sure
   response <- fix_datetime(response)
 
-  tibble::as_tibble(response)
+  as_tibble(response)
 }
