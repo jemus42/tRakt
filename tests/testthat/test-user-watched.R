98f11a246133f066f1b35ab1290fcfcaf457ac00
Lukas Burk <lukas@quantenbrot.de>
2019-09-04 18:58:41 +0200

    Fix test

--- tests/testthat/test-user-watched.R
+++ tests/testthat/test-user-watched.R
@@ -6,23 +6,20 @@ test_that("trakt.user.watched works", {
   user <- "jemus42"
 
   # shows ----
-  watched_shows <- trakt.user.watched(user = user, type = "shows")
-  watched_shows_full <- trakt.user.watched(user = user, type = "shows", extended = "full")
+  watched_shows <- trakt.user.watched(user = user, type = "shows", noseasons = TRUE)
+  watched_shows_full <- trakt.user.watched(user = user, type = "shows", noseasons = FALSE)
 
   expect_is(watched_shows, "tbl")
   expect_is(watched_shows_full, "tbl")
 
-  # shows.extended ----
-  watched_shows.ext <- trakt.user.watched(user = user, type = "shows", noseasons = FALSE)
+  expect_true(tibble::has_name(watched_shows_full, "seasons"))
 
-  expect_is(watched_shows.ext, "tbl")
-  expect_true(tibble::has_name(watched_shows.ext, "seasons"))
+  expect_equal(setdiff(names(watched_shows_full), names(watched_shows)), "seasons")
 
   # movies ----
   watched_movies <- trakt.user.watched(user = user, type = "movies")
 
   expect_is(watched_movies, "tbl")
-
 
   # error conditions ----
   expect_error(trakt.user.watched(user = user, type = "schnitzel"))
