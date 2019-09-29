8de20843f878c7f83791572bf3248da9b0ef794b
Lukas Burk <lukas@quantenbrot.de>
2019-09-29 16:47:35 +0200

    Tweak logo thing?

--- R/tRakt-package.R
+++ R/tRakt-package.R
@@ -1,3 +1,9 @@
-#' @importFrom tibble tibble
 #' @keywords internal
 "_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom tibble tibble
+## usethis namespace: end
+NULL
