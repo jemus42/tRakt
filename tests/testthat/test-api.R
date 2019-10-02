3cfe3579fa5c113d7c753b768605dd10e605e637
Lukas Burk <lukas@quantenbrot.de>
2019-10-02 20:27:43 +0200

    Fix a derp

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -1,18 +1,18 @@
 context("API interaction")
 
 test_that("Client ID is set without .Renviron", {
-  client.id <- "12fc1de7671c7f2fb4a8ac08ba7c9f45b447f4d5bad5e11e3490823d629afdf2"
+  client_id <- "12fc1de7671c7f2fb4a8ac08ba7c9f45b447f4d5bad5e11e3490823d629afdf2"
   expect_message(trakt_credentials(silent = FALSE))
 
   Sys.setenv(trakt_client_id = "")
   expect_failure(expect_message(trakt_credentials()))
-  expect_equal(getOption("trakt_client_id"), client.id)
+  expect_equal(getOption("trakt_client_id"), client_id)
 
   expect_message(
     trakt_credentials(username = "arbitraryusername", silent = FALSE)
   )
   expect_equal(getOption("trakt_username"), "arbitraryusername")
-  expect_message(trakt_credentials(client.id = client.id, silent = FALSE))
+  expect_message(trakt_credentials(client_id = client_id, silent = FALSE))
 })
 
 test_that("trakt_get can make API calls", {
