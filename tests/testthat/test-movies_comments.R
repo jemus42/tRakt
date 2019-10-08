3ea19bc0eb63d8e7f56de3338f9528d7e45d18eb
Lukas Burk <lukas@quantenbrot.de>
2019-10-08 18:46:41 +0200

    Fix test case

--- tests/testthat/test-movies_comments.R
+++ tests/testthat/test-movies_comments.R
@@ -17,13 +17,13 @@ test_that("media comments work", {
     nrow() %>%
     expect_lte(10)
 
-  seasons_comments(46241, season = 1, sort = "likes") %>%
+  seasons_comments(1388, season = 1, sort = "likes") %>%
     expect_is("tbl_df") %>%
     expect_named(media_comments_names_min) %>%
     nrow() %>%
     expect_lte(10)
 
-  episodes_comments(46241, season = 1, episode = 2, sort = "likes") %>%
+  episodes_comments(1388, season = 1, episode = 2, sort = "likes") %>%
     expect_is("tbl_df") %>%
     expect_named(media_comments_names_min) %>%
     nrow() %>%
