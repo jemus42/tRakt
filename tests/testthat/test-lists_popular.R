1c40f3ced783c85e88c098f925d772b3f85b9fb6
Lukas Burk <lukas@quantenbrot.de>
2019-10-08 19:53:33 +0200

    Test popular/trending lists

---
+++ tests/testthat/test-lists_popular.R
@@ -0,0 +1,20 @@
+test_that("Popular/trending lists work", {
+  list_names <- c(
+    "name", "description", "privacy", "display_numbers", "allow_comments",
+    "sort_by", "sort_how", "created_at", "updated_at", "item_count",
+    "comment_count", "likes", "trakt", "slug", "username", "private",
+    "user_name", "vip", "vip_ep", "user_slug"
+  )
+
+  lists_popular() %>%
+    expect_is("tbl_df") %>%
+    expect_named(list_names) %>%
+    nrow() %>%
+    expect_equal(10)
+
+  lists_trending() %>%
+    expect_is("tbl_df") %>%
+    expect_named(list_names) %>%
+    nrow() %>%
+    expect_equal(10)
+})
