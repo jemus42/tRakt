f89d0a7057ac2cc2462902694786fc47d2fd14fd
Lukas Burk <lukas@quantenbrot.de>
2019-10-08 02:01:15 +0200

    Add remaining list methods (closes #15)

---
+++ R/movies_lists.R
@@ -0,0 +1,173 @@
+#' Get lists containing a movie, show, season, episode or person
+#'
+#' @name media_lists
+#' @inheritParams trakt_api_common_parameters
+#' @param type `character(1) ["all"]`: The type of list, one of "all", "personal",
+#'   "official" or "watchlists".
+#' @param sort `character(1) ["popular"]`: Sort lists by one of "popular", "likes",
+#'   "comments", "items", "added" or "updated".
+#'
+#' @return A [tibble()][tibble::tibble-package].
+#' @importFrom rlang is_empty
+#' @importFrom dplyr bind_cols select
+#' @importFrom purrr pluck
+#' @examples
+#' \dontrun{
+#' movies_lists("190430", type = "personal", limit = 5)
+#' shows_lists("46241")
+#' seasons_lists("46241", season = 1)
+#' episodes_lists("46241", season = 1, episode = 1)
+#' people_lists("david-tennant")
+#' }
+NULL
+
+#' @export
+#' @describeIn media_lists Lists containing a movie.
+movies_lists <- function(id,
+                         type = c("all", "personal", "official", "watchlists"),
+                         sort = c("popular", "likes", "comments",
+                                  "items", "added", "updated"),
+                         limit = 10L,
+                         extended = c("min", "full")) {
+
+  type <- match.arg(type)
+  sort <- match.arg(sort)
+  extended <- match.arg(extended)
+
+  url <- build_trakt_url("movies", id, "lists", type, sort,
+                         limit = limit, extended = extended)
+  response <- trakt_get(url)
+
+  if (is_empty(response)) {
+    return(tibble())
+  }
+
+  response %>%
+    select(-"ids", -"user") %>%
+    bind_cols(
+      pluck(response, "ids") %>% fix_ids(),
+      pluck(response, "user") %>% unpack_user()
+    ) %>%
+    fix_tibble_response()
+}
+
+#' @export
+#' @describeIn media_lists Lists containing a show.
+shows_lists <- function(id,
+                         type = c("all", "personal", "official", "watchlists"),
+                         sort = c("popular", "likes", "comments",
+                                  "items", "added", "updated"),
+                         limit = 10L,
+                         extended = c("min", "full")) {
+
+  type <- match.arg(type)
+  sort <- match.arg(sort)
+  extended <- match.arg(extended)
+
+  url <- build_trakt_url("shows", id, "lists", type, sort,
+                         limit = limit, extended = extended)
+  response <- trakt_get(url)
+
+  if (is_empty(response)) {
+    return(tibble())
+  }
+
+  response %>%
+    select(-"ids", -"user") %>%
+    bind_cols(
+      pluck(response, "ids") %>% fix_ids(),
+      pluck(response, "user") %>% unpack_user()
+    ) %>%
+    fix_tibble_response()
+}
+
+#' @export
+#' @describeIn media_lists Lists containing a season.
+seasons_lists <- function(id, season,
+                         type = c("all", "personal", "official", "watchlists"),
+                         sort = c("popular", "likes", "comments",
+                                  "items", "added", "updated"),
+                         limit = 10L,
+                         extended = c("min", "full")) {
+
+  type <- match.arg(type)
+  sort <- match.arg(sort)
+  extended <- match.arg(extended)
+
+  url <- build_trakt_url("shows", id, "seasons", season, "lists", type, sort,
+                         limit = limit, extended = extended)
+  response <- trakt_get(url)
+
+  if (is_empty(response)) {
+    return(tibble())
+  }
+
+  response %>%
+    select(-"ids", -"user") %>%
+    bind_cols(
+      pluck(response, "ids") %>% fix_ids(),
+      pluck(response, "user") %>% unpack_user()
+    ) %>%
+    fix_tibble_response()
+}
+
+#' @export
+#' @describeIn media_lists Lists containing an episode.
+episodes_lists <- function(id, season, episode,
+                          type = c("all", "personal", "official", "watchlists"),
+                          sort = c("popular", "likes", "comments",
+                                   "items", "added", "updated"),
+                          limit = 10L,
+                          extended = c("min", "full")) {
+
+  type <- match.arg(type)
+  sort <- match.arg(sort)
+  extended <- match.arg(extended)
+
+  url <- build_trakt_url("shows", id, "seasons", season, "episodes", episode,
+                         "lists", type, sort,
+                         limit = limit, extended = extended)
+  response <- trakt_get(url)
+
+  if (is_empty(response)) {
+    return(tibble())
+  }
+
+  response %>%
+    select(-"ids", -"user") %>%
+    bind_cols(
+      pluck(response, "ids") %>% fix_ids(),
+      pluck(response, "user") %>% unpack_user()
+    ) %>%
+    fix_tibble_response()
+}
+
+#' @export
+#' @describeIn media_lists Lists containing a person.
+people_lists <- function(id,
+                         type = c("all", "personal", "official", "watchlists"),
+                         sort = c("popular", "likes", "comments",
+                                  "items", "added", "updated"),
+                         limit = 10L,
+                         extended = c("min", "full")) {
+
+  type <- match.arg(type)
+  sort <- match.arg(sort)
+  extended <- match.arg(extended)
+
+  url <- build_trakt_url("people", id, "lists", type, sort,
+                         limit = limit, extended = extended)
+  response <- trakt_get(url)
+
+  if (is_empty(response)) {
+    return(tibble())
+  }
+
+  response %>%
+    select(-"ids", -"user") %>%
+    bind_cols(
+      pluck(response, "ids") %>% fix_ids(),
+      pluck(response, "user") %>% unpack_user()
+    ) %>%
+    fix_tibble_response()
+}
