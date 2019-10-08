3b0c56d20fbec93e2554b607f85dc565bd137d1c
Lukas Burk <lukas@quantenbrot.de>
2019-10-08 16:52:18 +0200

    Test aliases

---
+++ tests/testthat/test-movies_aliases.R
@@ -0,0 +1,11 @@
+test_that("aliases work", {
+  movies_aliases(190430) %>%
+    expect_is("tbl_df") %>%
+    expect_length(2) %>%
+    expect_named(c("title", "country"))
+
+  shows_aliases(104439) %>%
+    expect_is("tbl_df") %>%
+    expect_length(2) %>%
+    expect_named(c("title", "country"))
+})
