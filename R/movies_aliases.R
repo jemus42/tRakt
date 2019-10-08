f33988db92d283cdb60eb4fb94776356d4ed4c3e
Lukas Burk <lukas@quantenbrot.de>
2019-10-08 16:44:39 +0200

    Add alias methods (closes #20)

---
+++ R/movies_aliases.R
@@ -0,0 +1,27 @@
+#' Get all movie / show aliases
+#'
+#' @inheritParams trakt_api_common_parameters
+#'
+#' @return A [tibble()][tibble::tibble-package].
+#' @export
+#'
+#' @examples
+#' movies_aliases(190430)
+#' shows_aliases(104439)
+movies_aliases <- function(id) {
+
+  url <- build_trakt_url("movies", id, "aliases")
+  response <- trakt_get(url)
+
+  as_tibble(response)
+}
+
+#' @rdname movies_aliases
+#' @export
+shows_aliases <- function(id) {
+
+  url <- build_trakt_url("shows", id, "aliases")
+  response <- trakt_get(url)
+
+  as_tibble(response)
+}
