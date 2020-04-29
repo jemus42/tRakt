ed50e98e30e5ca368065886bcadbcd979b826d0d
Lukas Burk <lukas@quantenbrot.de>
2020-04-29 04:22:46 +0200

    Test user_likes() (probably only locally)

---
+++ tests/testthat/test-user_likes.R
@@ -0,0 +1,17 @@
+skip_if_no_auth <- function() {
+  if (identical(Sys.getenv("trakt_client_secret"), "")) {
+    skip("No authentication available")
+  }
+}
+
+test_that("user_likes works", {
+  skip_if_no_auth()
+
+  user_likes(type = "comments") %>%
+    expect_is("tbl_df") %>%
+    expect_length(18)
+
+  user_likes(type = "lists") %>%
+    expect_is("tbl_df") %>%
+    expect_length(22)
+})
