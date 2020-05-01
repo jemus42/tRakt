8aba9543dc0a362373cf339899a3e88d0d28d190
Lukas Burk <lukas@quantenbrot.de>
2020-05-01 21:50:08 +0200

    Fix test

--- tests/testthat/test-user_comments.R
+++ tests/testthat/test-user_comments.R
@@ -3,10 +3,10 @@ test_that("user_comments", {
     "type", "id", "comment", "spoiler", "review", "parent_id",
     "created_at", "updated_at", "replies", "likes", "user_rating",
     "username", "private", "user_name", "vip", "vip_ep", "user_slug",
-    "title", "year", "trakt", "slug", "tvdb", "imdb", "tmdb", "season",
-    "episode", "episode_title", "episode_trakt", "episode_tvdb",
-    "episode_imdb", "episode_tmdb", "season_trakt", "season_tvdb",
-    "season_tmdb"
+    "title", "year", "trakt", "slug", "tvdb", "imdb", "tmdb", "episode_season",
+    "episode_number", "episode_title", "episode_trakt", "episode_tvdb",
+    "episode_imdb", "episode_tmdb", "season_number", "season_trakt",
+    "season_tvdb", "season_tmdb"
   )
 
   user_comments("jemus42") %>%
