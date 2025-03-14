31685d17085d0fd68723fb637689bdba30f6a3ba
Lukas Burk <lukas@quantenbrot.de>
2025-03-14 17:02:49 +0100

    nvmd

--- R/tRakt-package.R
+++ R/tRakt-package.R
@@ -4,8 +4,6 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @importFrom glue glue
-#' @importFrom lifecycle deprecated
 #' @importFrom tibble as_tibble
 #' @importFrom tibble tibble
 ## usethis namespace: end
